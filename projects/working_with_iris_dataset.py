c9f8ea6c3fe5c7596976ab7bd5b8f5fa41f71e1c
Jeffrey Burmood <mountainlifeissimple@icloud.com>
2023-06-22 04:46:42 -0600

    Update working_with_iris_dataset.py

--- projects/working_with_iris_dataset.py
+++ projects/working_with_iris_dataset.py
@@ -8,3 +8,17 @@ print(iris.feature_names)
 print(iris.target)
 
 print(iris.target_names)
+
+print(type(iris.data))
+
+print(type(iris.target))
+
+print(iris.data.shape)
+
+print(iris.target.shape)
+
+# store feature matrix in "X" - capitalized because X is a matrix
+X = iris.data
+
+# store response vector in "y" - lower case because y is a vector
+y = iris.target
