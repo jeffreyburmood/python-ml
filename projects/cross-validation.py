c74e0a6c2e7c4525e3334fedb8ee90231269eff5
Jeffrey Burmood <mountainlifeissimple@icloud.com>
2023-07-25 08:49:25 -0600

    Update cross-validation.py

--- projects/cross-validation.py
+++ projects/cross-validation.py
@@ -22,3 +22,15 @@ knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train, y_train)
 y_pred = knn.predict(X_test)
 print(f"train/test split accuracy = {metrics.accuracy_score(y_test, y_pred)}")
+
+# Now looking at K-fold cross-validation which essentially performs train/test split predictions K times
+# and averages out the results
+
+from sklearn.model_selection import KFold
+
+kf = KFold(n_splits=5, shuffle=False)
+
+for i, (train_index, test_index) in enumerate(kf.split(X)):
+    print(f"Fold {i}:")
+    print(f"  Train: index={train_index}")
+    print(f"  Test:  index={test_index}")
