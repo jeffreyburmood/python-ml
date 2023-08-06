9fe660c3a858c5254c0b4c1ee20ee51e27d269df
Jeffrey Burmood <mountainlifeissimple@icloud.com>
2023-08-06 07:37:20 -0600

    Update classification_model_evalutation.py
    Add confusion matrix topic

--- projects/classification_model_evalutation.py
+++ projects/classification_model_evalutation.py
@@ -70,3 +70,62 @@ print(y_test.value_counts().head(1) / len(y_test))
 # conclusion - classification accuracy does not take into account the underlying distribution of response values
 print(f"True: {y_test.values[0:25]}")
 print(f"Pred: {y_pred_class[0:25]}")
+
+###
+### Confusion Matrix - table that describes the performance of a classification model
+###
+# IMPORTANT: first argument is true values, second argument is predicted values
+confusion = metrics.confusion_matrix(y_test, y_pred_class)
+print(f"Confusion Matric: {confusion}")
+
+# Basic terminology
+# - True Positives (TP) - correctly predicted had diabetes
+# - True Negatives (TN) - correctly predicted did NOT have diabetes
+# - False Positives (FP) - incorrectly predicted had diabetes
+# - Flase Negatives (FN) - incorrectly predicted did NOT have diabetes
+
+# slice up the confusion matrix for calculations
+TP = confusion[1, 1]
+TN = confusion[0, 0]
+FP = confusion[0, 1]
+FN = confusion[1, 0]
+
+# Classification accuracy
+print(
+    f"classification accuracy from confusion matrix: {(TP + TN) / float(TP + TN + FP + FN)}"
+)
+print(
+    f"Classification accuracy calculated:            {metrics.accuracy_score(y_test, y_pred_class)}"
+)
+
+# Classification error
+print(
+    f"classification error from confusion matrix: {(FP + FN) / float(TP + TN + FP + FN)}"
+)
+print(
+    f"Classification error calculated:            { 1 - metrics.accuracy_score(y_test, y_pred_class)}"
+)
+
+# Sensitivity - when the actual value is positive, how often is the rediction correct?
+print(f"classification sensitivity from confusion matrix: {TP / float(TP + FN)}")
+print(
+    f"Classification sensitivty calculated:             {metrics.recall_score(y_test, y_pred_class)}"
+)
+
+# Specificity - when the actual value is negative, how often is the prediction correct?
+print(f"classification specificity from confusion matrix: {TN / float(TN + FP)}")
+
+# False Positive Rate - when the actual valuve is negative, how often is the prediction correct? (1-specificity)
+print(
+    f"classification false positive rate from confusion matrix: {FP / float(TN + FP)}"
+)
+
+# Precision - when the a positive value is predicted, how often is the prediction correct?
+print(f"classification precision from confusion matrix: {TP / float(TP + FP)}")
+print(
+    f"Classification precision calculated:             {metrics.precision_score(y_test, y_pred_class)}"
+)
+
+#
+# Adjusting the classification threshold
+#
