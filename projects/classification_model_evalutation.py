a8a6994e700908ab7b005bd78855ca108e64af17
Jeffrey Burmood <mountainlifeissimple@icloud.com>
2023-08-07 05:52:11 -0600

    Update classification_model_evalutation.py

--- projects/classification_model_evalutation.py
+++ projects/classification_model_evalutation.py
@@ -3,9 +3,7 @@
     2. What is the usage of classification accuracy?
     3. How does a confusion matrix describe the performance of a classifier?
     4. What metrics can be computed from a confusion matrix?
-    5. How can you adjust classifier performance by changing the classification threashold?
-    6. What is the purpose of the ROC curve?
-    7. How does the Area under the curve (AUC) differ from classification accuracy? """
+"""
 
 # Pima Indian Diabetes dataset
 import pandas as pd
