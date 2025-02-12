4d131b570ccd9c530fb39677a19330bfca518f6a
Jeffrey Burmood <Jeffrey-swdev@proton.me>
2025-02-12 06:51:16 -0700

    Add cross validation.

--- projects/master_scikit_learn/ch10_pipeline_eval_w_crossvalidation.py
+++ projects/master_scikit_learn/ch10_pipeline_eval_w_crossvalidation.py
@@ -39,3 +39,37 @@ pipe = make_pipeline(ct, logreg)
 pipe.fit(X, y)
 
 print(cross_val_score(pipe, X, y, cv=5, scoring='accuracy').mean())
+
+# parameters for GridSearchCV (as a dictionary)
+# - Key: step parameter
+# - Value: list of values to try
+
+params = {}
+params['logisticregression__penalty'] = ['l1', 'l2']
+params['logisticregression__C'] = [0.1, 1, 10]
+
+from sklearn.model_selection import GridSearchCV
+grid = GridSearchCV(pipe, params, cv=5, scoring='accuracy')
+grid.fit(X, y)
+
+results = pd.DataFrame(grid.cv_results_)
+print(results.sort_values('rank_test_score'))
+
+# Tuning the transformers
+print(pipe.named_steps['columntransformer'].named_transformers_)
+
+# show all pipeline parameters
+print(list(pipe.get_params().keys()))
+
+params['columntransformer__pipeline__onehotencoder__drop'] = [None, 'first']
+params['columntransformer__countvectorizer__ngram_range'] = [(1,1), (1,2)]
+params['columntransformer__simpleimputer__add_indicator'] = [False, True]
+
+grid = GridSearchCV(pipe, params, cv=5, scoring='accuracy')
+grid.fit(X, y)
+
+results = pd.DataFrame(grid.cv_results_)
+print(results.sort_values('rank_test_score'))
+
+print(grid.best_score_)
+print(grid.best_params_)
