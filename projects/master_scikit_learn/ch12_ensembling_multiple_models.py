834c637277037139cc35d68e7a936bef64233807
Jeffrey Burmood <Jeffrey-swdev@proton.me>
2025-03-03 07:15:17 -0700

    Add ensemble models.

---
+++ projects/master_scikit_learn/ch12_ensembling_multiple_models.py
@@ -0,0 +1,4 @@
+""" This file contains code related to ensembling , or combining, multiple models that is more accurate than a single model.
+    - Regression - average the predictions
+    - Classification - average the predicted probabilities or let the classifiers vote on the class """
+
